6f488957239ab6136a1902d223a1ce994716a2f0
CamileGhastine <camileghastine@hotmail.com>
2026-02-05 16:16:32 +0100

    chat

--- Module-4_bdd/exercices/docker-chat/docker/mysql/init.sql
+++ Module-4_bdd/exercices/docker-chat/docker/mysql/init.sql
@@ -14,3 +14,9 @@ INSERT INTO comment (content) VALUES
 ('L''important ce n''est pas ce que l''on a en soit, mais c''est ce que l''on sort de soit'),
 ('Qui ne tente rien n''a rien'),
 ('La patience est mère de toutes les vertus');
+
+CREATE TABLE user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    pseudo VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
