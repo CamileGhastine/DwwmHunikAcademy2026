245645e6f128d91e85dcf9dab3f3327aca457e45
CamileGhastine <camileghastine@hotmail.com>
2026-02-11 15:38:06 +0100

    correction projet 3 (wip)

--- Module-4_bdd/Projet/projet-3/app/src/add.php
+++ Module-4_bdd/Projet/projet-3/app/src/add.php
@@ -1,12 +1,54 @@
 <?php
-// 1) Si le formaulaire a été soumi, alors on récupère les saisies utilisateurs ($_POST)
-// 2) On les envoie dans la BDD
-// 
-// 3) Quand vous aures réussi le 1 et le 2. Il faudra vérifier les données saisies avant de les erngistrées.
-//    si les données sont valides alors on enregistre en bdd, sinon on envoie un message pour expliquer l'erreur. 
+//Si le formaulaire a été soumi
+if(!empty($_POST)) {
+// On récupère les saisies utilisateurs ($_POST)
+$title = $_POST['title'];
+$description = $_POST['description'];
+$urlGit = $_POST['url_git'];
+$userId = 1;
+
+// On vérifie si les chmaps comportenet des erreurs
+$message = fieldsVerify($title, $description, $userId);
+
+// Si pas d'erreur sur les chamsp on enregistre dans la BDD
+if (!$message) {
+    // 2) On les envoie dans la BDD
+    $pdo = new PDO('mysql:dbname=my_portfolio;host=mysql;charset=UTF8', 'user', 'pwd');
+    $sql = "INSERT INTO projects(title, description, url_git, user_id) VALUES (:title, :description, :urlGit, :userId)";
+    $request = $pdo->prepare($sql);
+    $request->execute([
+        'title' => $title,
+        'description' => $description,
+        'urlGit' => $urlGit,
+        'userId' => $userId
+    ]);
+}
 
 
 
+// 3) Quand vous aura réussi le 1 et le 2. Il faudra vérifier les données saisies avant de les erngistrées.
+//    si les données sont valides alors on enregistre en bdd, sinon on envoie un message pour expliquer l'erreur.
+
+
+// 3) Une fois les données enregistrée dans la BDD on rediriges vers la liste des projets.
+
+var_dump($message);
+
+
+}
+
+
+function fieldsVerify($title, $description, $userId) {
+    // Vérifier que les chmaps ne sont pas vides
+    if(empty($title) || empty($description) || empty($userId)) {
+        return 'Tous le champs doivent être renseignés.';
+    }
+
+    //  Vérifier que le titre à moins de 100 caractères
+
+    // Vérifier que la description à moins de 1000 caractère
+
+}
 
 ?>
 
@@ -40,13 +82,13 @@
 
        <form action="" method="post">
             <label for="title">Titre du projet</label>
-            <input type="text" name="title" maxlength="100" required>
+            <input type="text" name="title" maxlength="100">
             <label for="description">Description du projet</label>
-            <textarea name="description" rows="10" maxlength="1000" required></textarea>
+            <textarea name="description" rows="10" maxlength="1000"></textarea>
             <label for="url_git">URL github</label>
             <input type="url" name="url_git">
-            <label for="">Auteur du projet</label>
-            <input type="text">
+            <label for="user_id">Auteur du projet</label>
+            <input type="text" name="user_id">
             <input type="submit" value="Ajouter" class="button">
        </form>
     </section>
