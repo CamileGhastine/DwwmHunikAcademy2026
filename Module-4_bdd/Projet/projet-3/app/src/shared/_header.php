ad6c8af00deda3703c8b3e0a6aec2021474a6bcc
CamileGhastine <camileghastine@hotmail.com>
2026-02-13 12:01:47 +0100

    connexion/register (wip)

--- Module-4_bdd/Projet/projet-3/app/src/shared/_header.php
+++ Module-4_bdd/Projet/projet-3/app/src/shared/_header.php
@@ -4,8 +4,9 @@
         <nav>
             <ul class="nav-links">
                 <li><a href="/index.php">Accueil</a></li>
-                <li><a href="projects.php">Projets</a></li>
+                <li><a href="/src/projects.php">Projets</a></li>
                 <li><a href="#">Contact</a></li>
+                <li><a href="#">Connexion</a> / <a href="#">Enregistrement</a></li>
             </ul>
         </nav>
     </header>
