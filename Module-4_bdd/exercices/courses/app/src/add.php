80b71e9934afc4640cb145e9b0c939a7265cd97e
CamileGhastine <camileghastine@hotmail.com>
2026-02-16 10:14:06 +0100

    add : validation du champs name

--- Module-4_bdd/exercices/courses/app/src/add.php
+++ Module-4_bdd/exercices/courses/app/src/add.php
@@ -5,6 +5,11 @@ if (!empty($_POST)) {
    // Si le formulaire est soumis, récupérer la saisie de l'utilisateur
     $name = $_POST['item'];
 
+    if (empty($name) || strlen($name) > 50) {
+        header('Location: /index.php');
+        exit;
+    }
+
     // Enregistrer la saisie dans la BDD
     $pdo = new PDO('mysql:dbname=courses;host=mysql', 'user', 'pwd');
     $sql = "INSERT INTO product(name) VALUES (:name)";
