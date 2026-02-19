5d8f18de377e6420ed0fb5d7968c2d2a5216fded
CamileGhastine <camileghastine@hotmail.com>
2026-02-19 17:29:28 +0100

    poo

--- Module-6_POO/intro_poo/app/Model/Entity/Book.php
+++ Module-6_POO/intro_poo/app/Model/Entity/Book.php
@@ -5,3 +5,116 @@
  */
 
 //Définir la classe Book ici
+class Book
+{
+    private $title;
+    private $author;
+    private $genre;
+    private $description;
+    private $dateInstanciation;
+
+    public function __construct()
+    {
+        $this->setDateInstanciation(date('d/m/Y à h:i:s'));
+    }
+
+    /**
+     * Get the value of title
+     */ 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */ 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of author
+     */ 
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set the value of author
+     *
+     * @return  self
+     */ 
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of genre
+     */ 
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    /**
+     * Set the value of genre
+     *
+     * @return  self
+     */ 
+    public function setGenre($genre)
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of description
+     */ 
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @return  self
+     */ 
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dateInstanciation
+     */ 
+    public function getDateInstanciation()
+    {
+        return $this->dateInstanciation;
+    }
+
+    /**
+     * Set the value of dateInstanciation
+     *
+     * @return  self
+     */ 
+    public function setDateInstanciation($dateInstanciation)
+    {
+        $this->dateInstanciation = $dateInstanciation;
+
+        return $this;
+    }
+}
--- Module-6_POO/intro_poo/app/Model/Entity/Manga.php
+++ Module-6_POO/intro_poo/app/Model/Entity/Manga.php
@@ -5,3 +5,27 @@
  */
 
 // Ecrire la classe Manga ici
+class Manga extends Book
+{
+    private $illustrator;
+
+    /**
+     * Get the value of illustrator
+     */ 
+    public function getIllustrator()
+    {
+        return $this->illustrator;
+    }
+
+    /**
+     * Set the value of illustrator
+     *
+     * @return  self
+     */ 
+    public function setIllustrator($illustrator)
+    {
+        $this->illustrator = $illustrator;
+
+        return $this;
+    }
+}
