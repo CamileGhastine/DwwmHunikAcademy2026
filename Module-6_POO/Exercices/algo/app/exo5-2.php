7167156db5e5d417f3329509a3ddf2bda2453eb0
CamileGhastine <camileghastine@hotmail.com>
2026-03-05 14:26:44 +0100

    correction algo wip

--- Module-6_POO/Exercices/algo/app/exo5-2.php
+++ Module-6_POO/Exercices/algo/app/exo5-2.php
@@ -3,9 +3,34 @@
 // Phrase cachée : remplacer "chien" par des étoiles dans la phrases "Il fait un temps de chien."
 // Il fait un temps de chien. -> Il fait un temps de *****.
 
-$phrase = "Il fait un temps de chien";
+$phrase = "Il fait un temps de chien tout les jours.";
+$recherche = 'chien';
 
-// Coder ici ...
+// *** Méthode 1 ***
+// $tab = explode(' ', $phrase);
+// $phraseCachee = '';
+// foreach ($tab as $mot) {
+//    if ($mot === $recherche) {
+//        $phraseCachee .= str_repeat('*', mb_strlen($recherche)) . ' ';
+//    } else {
+//        $phraseCachee .= $mot . ' ';
+//    }
+// }
+
+
+//*** Méthode 2 ***
+// $tab = explode(' ', $phrase);
+
+// foreach ($tab as $key => $mot) {
+//     if ($mot === $recherche) {
+//         $tab[$key] = str_repeat('*', mb_strlen($recherche)) . ' ';
+//     }
+// }
+
+// $phraseCachee = implode(' ', $tab);
+
+// *** Méthode 3 ***
+$phraseCachee = str_replace($recherche, str_repeat('*', mb_strlen($recherche)), $phrase);
 
 
 echo "Phrase cachée : $phraseCachee";
--- Module-6_POO/Exercices/algo/app/exo5-3.php
+++ Module-6_POO/Exercices/algo/app/exo5-3.php
@@ -8,7 +8,13 @@ $carte1 = "1234567890123456";
 
 
 
-
-
 echo "Votre CB : $carteCachee";
+echo '<br>';
+
+// Numéro de CB caché : adapter le code au cas où le numéro de CB compte des esapce.
+// 1234 5678 9012 3456 -> ************2456
+$carte2 = '1234 5678 9012 3456';
+
+
+echo "Votre CB : $carteCachee2";
 echo '<br>';
