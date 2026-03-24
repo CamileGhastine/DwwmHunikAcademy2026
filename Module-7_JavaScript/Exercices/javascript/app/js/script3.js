55b0ba4b3708f9fb410b5274826b406744b1a8d0
CamileGhastine <camileghastine@hotmail.com>
2026-03-24 12:57:37 +0100

    exo 2 coorection wip

--- Module-7_JavaScript/Exercices/javascript/app/js/script3.js
+++ Module-7_JavaScript/Exercices/javascript/app/js/script3.js
@@ -1,35 +1,73 @@
-let form = document.querySelector('form');
-let firstname = document.getElementById('firstname');
-let zip = document.querySelector('#zip');
-let level = document.getElementById('level')
-let motivations = document.getElementsByName('motivation');
-let job = document.getElementById('job')
+const authorisedLevels = ['beginner', 'intermediate', 'advanced'];
+const authorisedMotivations = ['job', 'rich', 'fun']
+
 let motivationsUser = [];
 
+let form = document.querySelector('form');
+let inputFirstname = document.getElementById('firstname');
+let inputZip = document.querySelector('#zip');
+let inputLevel = document.getElementById('level')
+let inputsMotivations = document.getElementsByName('motivation');
+let inputAge = document.getElementById('age');
 
+// Gère la valdiation du nom à la volée
+inputFirstname.addEventListener('input', (event) => {
+    if (document.getElementById('firstnameMessage')) {
+        document.getElementById('firstnameMessage').remove();
+    }
+    if (event.target.value.length > 10) {
+        let firstnameMessage = document.createElement('p');
+        firstnameMessage.setAttribute('id', 'firstnameMessage')
+        firstnameMessage.textContent = "Le nom ne peut pas dépasser 10 caractères";
+        inputFirstname.after(firstnameMessage);
+    }
+})
+
+// Gère la validation e l'age au changement d'input
+inputAge.addEventListener('change', (event) => {
+    if (document.getElementById('ageMessage')) {
+        document.getElementById('ageMessage').remove();
+    }    if (event.target.value < 0 || event.target.value > 150) {
+        let ageMessage = document.createElement('p');
+        ageMessage.setAttribute('id', 'ageMessage')
+        ageMessage.textContent = "L'age doit être compris entre 0 et 150";
+        inputAge.after(ageMessage);
+    }
+})
+
+// Gère tous les champs à la soumission
 form.addEventListener('submit', (event) => {
     event.preventDefault();
+
     let message = "";
     let error = document.getElementById('error');
+    let motivations = [ ...authorisedMotivations];
 
     if (error) error.remove();
-    let name = firstname.value
-    let age = form.age.value
+    let name = inputFirstname.value;
+    let age = inputAge.value;
+    let level = inputLevel.value;
 
-    console.log(zip.value)
-    console.log(level.value)
-    if(job.checked) {
-        console.log(job.value)
-    }
-    motivations.forEach((motivation) => {
+    message = handleZipCode();
+
+    //GERE LES MOTIVATIONS
+    inputsMotivations.forEach((motivation) => {
         if (motivation.checked) {
-            motivationsUser.push(motivation.value);
+            let index = authorisedMotivations.indexOf(motivation.value);
+            if (index < 0) {
+                message = "Vos motivations pour apprendre le JavaScript ne sont pas autorisées."
+            } else {
+                motivations.splice(index, 1)
+                motivationsUser.push(motivation.value);
+            }
         }
     })
-    console.log(motivationsUser)
 
-    
+    if (authorisedLevels.indexOf(level) < 0) {
+        message = "Le niveau choisi n'est pas autorisé."
+    }
 
+    // GERE AGE
     if (age === "") {
         message = "L'age doit être rempli";
     } else if (!Number(age)) {
@@ -38,19 +76,25 @@ form.addEventListener('submit', (event) => {
         message = "L'age doit être compris entre 0 et 150";
     }
 
+    // GERE LE NOM
     if (name === "") {
         message = "Le nom doit être rempli";
     } else if (name.length > 10) {
         message = "Le nom ne peut pas dépasser 10 caractères";
     }
 
-
+    // GERE LES ERREURS
     if (message != "") {
         let errorMessage = document.createElement('p');
         errorMessage.textContent = message;
         errorMessage.setAttribute('id', 'error')
         document.querySelector('button').after(errorMessage)
     }
-
 })
 
+function handleZipCode() {
+    let regex = new RegExp("^[0-9]{5}$");
+    if(!regex.test(inputZip.value)) {
+        return "Le code postal n'est pas valide."
+    }
+}
